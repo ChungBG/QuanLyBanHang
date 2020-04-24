215de2156e8576f1f5d183fe814bf857f9cbf98c
ChungBG <64247988+ChungBG@users.noreply.github.com>
2020-04-24 14:14:09 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Chuong3part1_Bai6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DanhSachSV htttak21 = new DanhSachSV();
+            htttak21.Nhap();
+            htttak21.Xuat();
+            htttak21.SapXep();
+            htttak21.Xuat();
+            Console.ReadLine();
+        }
+    }
+}
